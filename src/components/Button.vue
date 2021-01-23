0e96ba4e7ea8190338ee58b4840934c004d866cd
Cynthiaaa <cynthia.verger@gmail.com>
2021-01-23 22:29:40 +0100

    fixed errors + added Coin Details button

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="buttonClick" class="bg-transparent hover:bg-indigo-200 p-2 text-indigo-700 font-semibold hover:text-white">
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  name: "Button",
+
+  methods: {
+    buttonClick() {
+      this.$emit('coin-click')
+    }
+  }
+}
+</script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
        <nav>
            <nav class="flex items-center justify-between flex-wrap bg-indigo-800 p-6">
                <div class="flex items-center flex-shrink-0 text-white mr-6">
-                   <span class="font-semibold text-xl tracking-tight">Coin Market</span>
+                   <router-link :to="{ name: 'home'}" class="font-semibold text-xl tracking-tight">Coin Market</router-link>
                </div>
                <div class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:wauto">
                    <div class="text-sm lg:flex-grow"></div>
