471a821b2d1add7632275e14c8e935aa52e19714
Cynthiaaa <cynthia.verger@gmail.com>
2021-01-24 21:38:43 +0100

    fixed button design

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,7 +1,9 @@
 <template>
-  <button @click="buttonClick" class="bg-transparent hover:bg-indigo-200 p-2 text-indigo-700 font-semibold hover:text-white">
+  <button @click="buttonClick" class="bg-transparent hover:bg-indigo-200 text-indigo-700 font-semibold hover:text-white">
     <beat-loader :loading="isLoading" :color="'#68d391'" :size="8" />
-    <slot v-show="isLoading"></slot>
+    <p v-show="!isLoading">
+      <slot></slot>
+    </p>
   </button>
 </template>
 <script>
